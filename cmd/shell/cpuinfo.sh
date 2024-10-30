5051852fa580e98df25389af3547d14169e9a9fc
zhangliuyang <zhangliuyang@unicloud.com>
2024-10-30 11:27:30 +0800

    fix: cpuinfo.sh 输出内容

--- cmd/shell/cpuinfo.sh
+++ cmd/shell/cpuinfo.sh
@@ -3,6 +3,9 @@
 # 执行cat /proc/cpuinfo并读取输出
 cpuinfo=$(cat /proc/cpuinfo)
 
+# 标志变量，用于输出HELP和TYPE行
+output_header=false
+
 # 使用循环处理每个处理器的信息
 while IFS= read -r line; do
     if [[ $line == "processor"* ]]; then
@@ -23,9 +26,13 @@ while IFS= read -r line; do
 
         # 如果找到了core id和cpu MHz，输出结果
         if [[ -n $core_id && -n $cpu_mhz ]]; then
-            echo "# HELP node_cpu_frequency_hertz Current frequency of the CPU in hertz."
-            echo "# TYPE node_cpu_frequency_hertz gauge"
-            echo "node_cpu_frequency_hertz{core=\"$core_id\"} $cpu_mhz"
+            # 只输出一次HELP和TYPE
+            if ! $output_header; then
+                echo "# HELP node_cpu_frequency_hertz Current frequency of the CPU in hertz."
+                echo "# TYPE node_cpu_frequency_hertz gauge"
+                output_header=true
+            fi
+            echo "node_cpu_frequency_hertz{core=\"$core_id\",processor_id=\"$processor_id\"} $cpu_mhz"
         fi
     fi
 done <<< "$cpuinfo"
